eae9676480019ee17c3bf8acac4ee4a360ac837e
Nicky Cheng <ncheng25@my.bcit.ca>
2024-10-08 20:09:41 -0700

    Debugged Naudio not importing\

--- Assets/FastFourierTransform.cs
+++ Assets/FastFourierTransform.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using 
 
 public class FastFourierTransform : MonoBehaviour
 {
+    readonly double[] AudioValues;
     // Start is called before the first frame update
     void Start()
     {
