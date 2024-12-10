09493685da7dc3ddc98d75c99f199c95dd1809e6
Nicky Cheng <90591673+RealGoldenGeneral@users.noreply.github.com>
2024-12-09 18:59:38 -0800

    Added wave generation

--- Assets/CameraSideScrolling.cs
+++ Assets/CameraSideScrolling.cs
@@ -6,11 +6,13 @@ public class CameraSideScrolling : MonoBehaviour
 {
     GameObject gameCamera;
     AudioSource source;
+    GameObject levelGenerator;
     // Start is called before the first frame update
     void Start()
     {
         gameCamera = GameObject.Find("Main Camera");
         source = GameObject.Find("Audio Source").GetComponent<AudioSource>();
+        levelGenerator = GameObject.Find("Level Generator");
     }
 
     // Update is called once per frame
@@ -19,6 +21,7 @@ public class CameraSideScrolling : MonoBehaviour
         if (source.isPlaying)
         {
             gameCamera.transform.position = new Vector3(gameCamera.transform.position.x + 0.1f, gameCamera.transform.position.y, gameCamera.transform.position.z);
+            levelGenerator.transform.position = new Vector3(levelGenerator.transform.position.x + 0.1f, levelGenerator.transform.position.y, levelGenerator.transform.position.z);
         }
     }
 }
