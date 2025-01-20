d976e9ba28a088fa1e5cf96dd5e0c05e60714510
Nicky Cheng <90591673+RealGoldenGeneral@users.noreply.github.com>
2025-01-20 15:13:47 -0800

    Replaced prefab cube with mesh

--- Assets/LevelGeneration.cs
+++ Assets/LevelGeneration.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class LevelGeneration : MonoBehaviour
 {
     public GameObject _sampleCubePrefab;
-    private GameObject[] _sampleCube;
+    private GameObject[] _bars;
     private float[] samples;
     public float _maxScale;
     private float _barScale;
@@ -16,6 +16,46 @@ public class LevelGeneration : MonoBehaviour
     private bool _direction; // 1 if upwards, 0 if downwards
     private float _increment;
     private int _updateCount;
+    public Material _barMaterial;
+
+    void CreateMesh(Mesh mesh, float x, float y, float z, float ymax)
+    {
+        mesh.Clear();
+        Vector3[] vertices = new Vector3[]
+       {
+            new Vector3 (0, 0, 0),
+            new Vector3 (0, 0, 1),
+            new Vector3 (1, 0, 0),
+            new Vector3 (1, 0, 1),
+            new Vector3 (0, 1, 0),
+            new Vector3 (0, 1, 1),
+            new Vector3 (1, 1, 0),
+            new Vector3 (1, 1, 1)
+       };
+
+        int[] triangles = new int[]
+        {
+            0, 1, 2,
+            2, 1, 3,
+            0, 1, 5,
+            5, 4, 0,
+            0, 2, 6,
+            6, 4, 0,
+            1, 3, 7,
+            7, 5, 1,
+            2, 3, 7,
+            7, 6, 2,
+            4, 5, 6,
+            6, 5, 7
+        };
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,17 +66,31 @@ public class LevelGeneration : MonoBehaviour
         _updateCount = 0;
         _increment = 0.0000000001f;
         _barScale = 0.3f / (TransformAudio._samples.Length / 64);
-        _sampleCube = new GameObject[TransformAudio._samples.Length];
+        _bars = new GameObject[TransformAudio._samples.Length];
         samples = new float[TransformAudio._samples.Length];
         for (int i = 0; i < TransformAudio._samples.Length; i++)
         {
-            GameObject _instanceSampleCube = (GameObject)Instantiate(_sampleCubePrefab);
-            _instanceSampleCube.transform.position = this.transform.position;
-            _instanceSampleCube.transform.parent = this.transform;
-            _instanceSampleCube.name = "SampleCube" + i;
+            //GameObject _instanceSampleCube = (GameObject)Instantiate(_sampleCubePrefab);
+            //_instanceSampleCube.transform.position = this.transform.position;
+            //_instanceSampleCube.transform.parent = this.transform;
+            //_instanceSampleCube.name = "SampleCube" + i;
+            //this.transform.position = new Vector3(i * _barScale * -1, 0, 0);
+            //_instanceSampleCube.transform.position = new Vector3(9.9f, -4.5f, 0);
+            //_sampleCube[i] = _instanceSampleCube;
+
+            GameObject _bar = new GameObject();
+            _bar.AddComponent<MeshFilter>();
+            _bar.AddComponent<MeshRenderer>();
+            Mesh _barMesh = new Mesh();
+            _bar.GetComponent<MeshFilter>().mesh = _barMesh;
+            _bar.GetComponent<MeshRenderer>().material = _barMaterial;
+            CreateMesh(_barMesh, 0, 0, 0, 0);
+            _bar.transform.position = this.transform.position;
+            _bar.transform.parent = this.transform;
+            _bar.name = "Bar" + i;
             this.transform.position = new Vector3(i * _barScale * -1, 0, 0);
-            _instanceSampleCube.transform.position = new Vector3(9.9f, -4.5f, 0);
-            _sampleCube[i] = _instanceSampleCube;
+            _bar.transform.position = new Vector3(9.9f, -4.5f, 0);
+            _bars[i] = _bar;
         }
     }
 
@@ -83,7 +137,7 @@ public class LevelGeneration : MonoBehaviour
             _sampleCount++;
             for (int j = samples.Length - 1; j >= 0; j--)
             {
-                _sampleCube[j].transform.localScale = new Vector3(_barScale, (samples[j] * _maxScale), _barScale);
+                _bars[j].transform.localScale = new Vector3(_barScale, (samples[j] * _maxScale), _barScale);
             }
         }
         _updateCount++;
