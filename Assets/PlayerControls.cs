ce37caa0bf95bda726e7912e458187034313c664
Nicky Cheng <90591673+RealGoldenGeneral@users.noreply.github.com>
2025-02-03 16:06:34 -0800

    Added rotation controls

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,28 +9,51 @@ public class PlayerControls : MonoBehaviour
     [SerializeField]
     public GameObject player;
     private Rigidbody rb;
+    private bool jumped;
     // Start is called before the first frame update
     void Start()
     {
         rb = player.GetComponent<Rigidbody>();
+        jumped = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Accelerate
         if (Input.GetKey(KeyCode.W) && rb.velocity.x < 15)
         {
             rb.AddForce(Vector3.right, ForceMode.Acceleration);
         }
 
+        // Brake
         if (Input.GetKey(KeyCode.S) && rb.velocity.x > 0)
         {
             rb.AddForce(Vector3.left, ForceMode.Acceleration);
         }
 
-        if (Input.GetKey(KeyCode.Space) && player.transform.position.y < -4.19f)
+        // Jump
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(Vector3.up * 75);
+            rb.AddForce(Vector3.up * 400);
+            jumped = true;
+        }
+
+        // Rotate Forwards
+        if (Input.GetKey(KeyCode.A) && jumped == true)
+        {
+            player.transform.Rotate(Vector3.forward * 180 * Time.deltaTime);
+        }
+        
+        // Rotate Backwards
+        if (Input.GetKey(KeyCode.D) && jumped == true)
+        {
+            player.transform.Rotate(Vector3.back * 180 * Time.deltaTime);
+        }
+
+        if (player.transform.position.y < -4.199f)
+        {
+            jumped = !jumped;
         }
     }
 }
