424789aebe9dc11b45ab59233abce97254fe0728
Nicky Cheng <90591673+RealGoldenGeneral@users.noreply.github.com>
2025-03-01 16:06:19 -0800

    Modified collision detection

--- Assets/AIBehaviour.cs
+++ Assets/AIBehaviour.cs
@@ -28,8 +28,9 @@ public class AIBehaviour : MonoBehaviour
     private void FixedUpdate()
     {
         RaycastHit hit;
+        LayerMask mask = LayerMask.GetMask("Collide Only With Water");
 
-        if (Physics.Raycast(AIPlayer.transform.position, Vector3.right, out hit))
+        if (Physics.Raycast(AIPlayer.transform.position, Vector3.right, out hit, ~mask))
         {
             if (hit.distance < 1 && !jumped)
             {
@@ -39,7 +40,7 @@ public class AIBehaviour : MonoBehaviour
             }
         }
 
-        if (Physics.Raycast(AIPlayer.transform.position, Vector3.down, out hit))
+        if (Physics.Raycast(AIPlayer.transform.position, Vector3.down, out hit, ~mask))
         {
             if (hit.distance > 1 && jumped)
             {
