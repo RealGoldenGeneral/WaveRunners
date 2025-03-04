3744b3496e31121531b132af0feedd0e39c0798a
Nicky Cheng <90591673+RealGoldenGeneral@users.noreply.github.com>
2025-03-03 18:24:29 -0800

    Added insert audio functionality

--- Assets/InsertAudio.cs
+++ Assets/InsertAudio.cs
@@ -1,11 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using SFB;
+using UnityEngine.Networking;
+using UnityEngine.EventSystems;
 public class InsertAudio : MonoBehaviour
 {
-    void onClick()
-    {
+    private AudioClip audioClip;
+    public GameObject gameManager;
+    public AudioSource audioSource;
+    public GameObject player;
+    public GameObject AI;
+    public Button button;
 
+    public void OnPointerDown(PointerEventData eventData) { }
+    private void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(onClick);
+    }
+    private void onClick()
+    {
+        var paths = StandaloneFileBrowser.OpenFilePanel("Select Audio", "", "mp3", false);
+        if (paths.Length > 0)
+        {
+            StartCoroutine(OutputRoutine(new System.Uri(paths[0]).AbsoluteUri));
+        }
+    }
+
+    private IEnumerator OutputRoutine(string url)
+    {
+        var loader = UnityWebRequestMultimedia.GetAudioClip(url, AudioType.MPEG);
+        yield return loader.SendWebRequest();
+        audioClip = DownloadHandlerAudioClip.GetContent(loader);
+        gameManager.SetActive(true);
+        player.SetActive(true);
+        AI.SetActive(true);
+        audioSource.clip = audioClip;
+        button.gameObject.SetActive(false);
+        audioSource.Play();
     }
 }
